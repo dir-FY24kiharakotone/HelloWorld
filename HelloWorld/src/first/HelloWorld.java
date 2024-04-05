9243d6d2305f8e39134b32e82023f55f8fb85d33
KotoneKihara(木原琴音) <kotone.kihara.a426@dirbato.co.jp>
2024-04-05 16:00:44 +0900

    原因：ローカルリポジトリ作成の際に、新規作成ではなく既存のリポジトリから選んでしまった。ローカルリポジトリ名とGitのリモートリポジトリ名が合っているかを要チェック

---
+++ HelloWorld/src/first/HelloWorld.java
@@ -0,0 +1,12 @@
+package first;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // TODO 自動生成されたメソッド・スタブ
+        
+        System.out.println("Hello, World");
+
+    }
+
+}
